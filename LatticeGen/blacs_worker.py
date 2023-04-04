4a70c41a9fb020c878fede43bff613ad7f33a62f
Chagab <gabriel.chatelain@protonmail.com>
2023-04-04 13:23:08 -0700

    implementing transition to buffered

--- LatticeGen/blacs_worker.py
+++ LatticeGen/blacs_worker.py
@@ -1,5 +1,6 @@
 from blacs.tab_base_classes import Worker
-
+import labscript_utils.h5_lock
+import h5py
 
 class LatticeGenWorker(Worker):
 
@@ -18,11 +19,25 @@ class LatticeGenWorker(Worker):
         BLACS_tab widgets to reflect these values."""
         pass
 
-    def transition_to_buffered(self):
+    def transition_to_buffered(self, device_name, h5_file, front_panel_values, refresh):
         """This method transitions the device to buffered shot mode, reading the
         shot h5 file and taking the saved instructions from
         labscript_device.generate_code and sending the appropriate commands to
         the hardware."""
+        self.shot_file = h5_file  # We'll need this in transition_to_manual
+        with h5py.File(self.shot_file, 'r') as hdf5_file:
+            group = hdf5_file[f'devices/{self.device_name}']
+            if 'START_COMMANDS' in group:
+                start_commands = self.hdf5_group_to_dict(group)
+            else:
+                start_commands = None
+
+        for command, argument in start_commands:
+            print(f'sending command: {command}')
+            if argument is None:
+                getattr(self, command)()
+            else: 
+                getattr(self, command)(**argument)
         return {}
 
     def transition_to_manual(self):
@@ -31,3 +46,24 @@ class LatticeGenWorker(Worker):
         and is used to read any measurements and save them to the shot h5 file
         as results."""
         return True
+    
+
+    def hdf5_group_to_dict(self, group):
+        """Helper functions that convert a hdf5 group to a dict which keys are 
+        functions to excute and values the corresponding arguments."""
+        result = {}
+        for key, value in group.attrs.items():
+            if isinstance(value, bytes):
+                value = value.decode('utf-8')
+            result[key] = value
+        for key, value in group.items():
+            if isinstance(value, h5py.Group):
+                result[key] = self.hdf5_group_to_dict(value)
+            else:
+                result[key] = list(value) if \
+                isinstance(value, h5py.Dataset) and len(value.shape) == 1 else value[()]
+        return result
+
+
+    def test(self, arg):
+        print(arg)
--- LatticeGen/labscript_devices.py
+++ LatticeGen/labscript_devices.py
@@ -26,6 +26,33 @@ class LatticeGen(Device):
         )
         self.name = name
         self.BLACS_connection = addr
+        self.start_commands = []
+    
+    def dict_to_hdf5_group(self, group, dictionary):
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                subgroup = group.create_group(key)
+                self.dict_to_hdf5_group(subgroup, value)
+            else:
+                if isinstance(value, list):
+                    value = tuple(value)
+                group.attrs[key] = value
+    
+    def add_start_command(self, command):
+        """Add a serial command that should be send at the start of the experiment"""
+        if not isinstance(command, bytes):
+            raise TypeError("command must be a bytestring")
+        self.start_commands.append(command)
+    
+
+    def test(self, arg):
+        self.add_start_command({
+            'test': arg
+        })
 
     def generate_code(self, hdf5_file):
-        Device.generate_code(self, hdf5_file)
+        group = self.init_device_group(hdf5_file)
+        if len(self.start_commands) > 0:
+            self.dict_to_hdf5_group(group, self.start_commands)
+        else:
+            print("No start commands")
